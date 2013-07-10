05f72ec0c4b9e75f955637c2dc3c89424414d471
Trevor Runcie <trevor.runcie@agilekm.com>
2013-07-10 12:13:46 +0000

    Updated web.js 13

--- web.js
+++ web.js
@@ -8,7 +8,6 @@ var buffer = new Buffer(255);
 
 fs.readFile('./index.html', function (err, data) {
   if (err) throw err;
-  buffer.write(data, "utf-8");
 });
 
 app.get('/', function(request, response) {
